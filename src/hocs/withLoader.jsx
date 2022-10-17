83d6b5f269f116c2fa71d276c5d2101aa9b51707
Aivaras <stasiulis.aivaras@gmail.com>
2022-10-18 00:03:56 +0300

    Updated withLoader HOC

--- src/hocs/withLoader.jsx
+++ src/hocs/withLoader.jsx
@@ -1,14 +1,36 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import Spinner from '../components/Spinner';
+import styled from 'styled-components';
+import { CgPokemon } from 'react-icons/cg';
 
 function withLoader(Component) {
   return function withLoaderHoc(props) {
     if (props.loading) {
-      return <Spinner />;
+      return (
+        <Loader>
+          <CgPokemon size={64} />
+        </Loader>
+      );
     }
 
     return <Component {...props} />;
   };
 }
+
+const Loader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    animation: rotate;
+    animation-duration: 1000ms;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default withLoader;
